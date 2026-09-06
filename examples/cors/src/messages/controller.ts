f1399faa781ea32a2217b044018ed709e62f01a5
yu <git@come25136.id>
2026-09-06 09:16:13 +0900

    fix(example): CORSの明示OPTIONS handlerを不要にする

--- examples/cors/src/messages/controller.ts
+++ examples/cors/src/messages/controller.ts
@@ -5,9 +5,6 @@ export const MessageController = http.implementation({
   name: 'MessageController',
   contract: MessageContract,
   factory: () => ({
-    preflight(ctx) {
-      return ctx.response.ok({})
-    },
     create(ctx) {
       return ctx.response.created({
         body: {
